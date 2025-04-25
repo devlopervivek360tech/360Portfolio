951ebd848ed1fe1d510e99d3d258be855df6b6c2
developervivek360tech <vivekraut.360tech@gmail.com>
2025-04-25 14:43:02 +0530

    Added theme colores

--- 360customizer-profile/src/components/HeroSection/ThreeDModel.jsx
+++ 360customizer-profile/src/components/HeroSection/ThreeDModel.jsx
@@ -4,7 +4,7 @@ import { useGLTF } from "@react-three/drei";
 export default function ThreeDModel() {
   // Update the path to point to public/models
   const { scene } = useGLTF("/models/shirt_model.glb");
-  return <primitive object={scene} scale={1.5} position={[0, -1, 0]} />;
+  return <primitive object={scene} scale={3.5} position={[0, -1, 0]} />;
 }
 
 // Add this at the bottom of the file to preload the model
